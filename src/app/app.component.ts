1179511ab86c540393437a107b9fc6d0a75f20c5
Aditya Deshmukh <adityadeshmukh7350@gmail.com>
2024-02-23 01:43:10 +0530

    sfdfd

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,57 @@
-import { Component } from '@angular/core';
+import {ChangeDetectionStrategy, Component, Inject} from '@angular/core';
+import {TuiAlertService} from '@taiga-ui/core';
+
+
+
+interface Item {
+  badge?: number;
+  icon: string;
+  text: string;
+}
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush,
+
 })
+
 export class AppComponent {
   title = 'IncomeExpense';
+  activeItemIndex = 1;
+ 
+  readonly items = [
+      {
+          text: 'Favorites',
+          icon: 'tuiIconHeartLarge',
+          badge: 3,
+      },
+      {
+          text: 'Calls',
+          icon: 'tuiIconPhoneLarge',
+          badge: 1234,
+      },
+      {
+          text: 'Profile',
+          icon: 'tuiIconUserLarge',
+      },
+      {
+          text: 'Settings and configuration',
+          icon: 'tuiIconSettingsLarge',
+          badge: 100,
+      },
+      {
+          text: 'More',
+          icon: 'tuiIconMoreHorizontalLarge',
+      },
+  ];
+
+  constructor(@Inject(TuiAlertService) private readonly alerts: TuiAlertService) {}
+
+
+  onClick(item: Item): void {
+      item.badge = 0;
+      this.alerts.open(this.activeItemIndex, {label: item.text}).subscribe();
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,10 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations'
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TuiModeModule, TuiRootModule, TuiThemeNightModule } from '@taiga-ui/core';
+import {TuiTabBarModule} from '@taiga-ui/addon-mobile';
 
 @NgModule({
   declarations: [
@@ -10,7 +12,12 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule, 
+    TuiRootModule,
+    TuiTabBarModule,
+    TuiThemeNightModule,
+    TuiModeModule
   ],
   providers: [],
   bootstrap: [AppComponent]
