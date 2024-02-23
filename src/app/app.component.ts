8c58217c4d714c5fd9f82fc0746836bdb00e5b77
Aditya Deshmukh <adityadeshmukh7350@gmail.com>
2024-02-24 01:37:38 +0530

    footer hide unhide done

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import {ChangeDetectionStrategy, Component, Inject} from '@angular/core';
+import { NavigationStart, Router } from '@angular/router';
 import {TuiAlertService} from '@taiga-ui/core';
 
 
@@ -18,7 +19,7 @@ interface Item {
 export class AppComponent {
   title = 'IncomeExpense';
   activeItemIndex = 1;
- 
+    isLoggedIn:boolean;
   readonly items = [
       {
           text: 'Records',
@@ -47,8 +48,19 @@ export class AppComponent {
       },
   ];
 
-  constructor(@Inject(TuiAlertService) private readonly alerts: TuiAlertService) {}
-
+  constructor(@Inject(TuiAlertService) private readonly alerts: TuiAlertService,private router: Router) {
+    router.events.forEach((event) => {
+        if (event instanceof NavigationStart) {
+          if (event['url'] == '/login' || event['url'] == '/register') {
+            this.isLoggedIn = false;
+          } else {
+            // console.log("NU")
+            this.isLoggedIn = true;
+          }
+        }
+      });
+  }
+ 
 
   onClick(item: Item): void {
       item.badge = 0;
